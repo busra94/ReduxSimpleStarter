a77f71735fe11964cc13d12b0a05e943f366c3d2
busra94@ <doganbusra94@gmail.com>
2018-10-31 17:53:58 +0300

    intro to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -15,16 +15,27 @@ some abilities like tell other parts of the application
 TO MAKE POSSIBLE THIS WE CAN USE CLASS COMPONENTS*/
 
 
- class SearchBar extends Component { // Give SearchBar class access to all of the functionality that React.Component has.
-    // render is a must method, others from Component are optional.
+class SearchBar extends Component { // Give SearchBar class access to all of the functionality that React.Component has.
+    constructor(props) { // all javascript classes have constructor, and it is the first and only function called automatically whenever a new instance of the class created. It is reserved for doing some setup or initializing variables.
+        super(props); // Component itself has its own constructor function. When we define that is already defined on the parent class which is Component, we can call that parent method on the parent class by calling super(). 
 
-    render(){ // if we do not return anything from render it's gonna give an error.
-        return <input onChange={
-            event => // if there is one argument we can drop parentheses
-            console.log(event.target.value) 
-         } />; // onChange() is a react event                         
+        this.state = { term: '' }; // only inside of the constructor function we can change state using 'this.state', except constructor function we instead of using this.setState = ... 
     }
-           
+    // render is a must method, others from Component are optional.
+    render() { // if we do not return anything from render it's gonna give an error.
+        return (
+            <div>
+                <input onChange={
+                    event => // if there is one argument we can drop parentheses
+                        this.setState(
+                            { term: event.target.value }
+                            )}
+                />
+{/*                 Value of input: {this.state.term}  we are not modifying term we are just referencing it */}
+            </div>
+        ); // onChange() is a react event                         
+    }
+
 }
 
 // React components can show other components, so because of there is no connection between components we need explicitly declare a connection between them.
@@ -48,3 +59,4 @@ because we want these abilities we use class component instead of function compo
     onInputChange(event){ // an event object describes the context or information about the event that occurred.        
     }  
 */
+
