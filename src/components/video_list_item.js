46a3c847a1044b034d0126e5bf2a94891a6174c5
busra94@ <doganbusra94@gmail.com>
2018-11-02 17:55:28 +0300

    video list items

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,14 @@
 // list  items
 import React from "react";
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => { // in here with curly braces we say the first object in the arguments has a property video, grab that video and declare a new variable called video
+    console.log(video);
     return <li>Video</li>
 };
 
 export default VideoListItem;
+
+
+/* (props) => {
+    const video = props.video;
+} */
